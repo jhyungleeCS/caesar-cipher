2bca01ac10c094deabaef75476d6292211d0758a
Daniel Lee <104159089+jhyungleeCS@users.noreply.github.com>
2024-06-11 02:22:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,52 @@
-def caesar(): 
-    message = input("What message do you want encrypted?: ")
-caesar()
+def encrypt(text, shift_level): 
+    uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercases = 'abcdefghijklmnopqrstuvwxyz'
+    
+    text_output = ""
+    for character in text:
+        if character in uppercases: 
+            original_pos = uppercases.index(character)
+            new_pos = (original_pos + shift_level) % 26
+            new_char = uppercases[new_pos]
+            text_output += new_char
+            
+        elif character in lowercases:
+            original_pos = lowercases.index(character)
+            new_pos = (original_pos + shift_level) % 26
+            new_char = lowercases[new_pos]
+            text_output += new_char
+            
+        else: 
+            text_output += character
+    return text_output
+
+def decrypt(text, shift_level): 
+    uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercases = 'abcdefghijklmnopqrstuvwxyz'
+    
+    text_output = ""
+    
+    for character in text: 
+        if character in uppercases: 
+            original_pos = uppercases.index(character) 
+            new_pos = (original_pos - shift_level) % 26
+            new_char = uppercases[new_pos]
+            text_output += new_char
+            
+        elif character in lowercases: 
+            original_pos = lowercases.index(character) 
+            new_pos = (original_pos - shift_level) % 26
+            new_char = lowercases[new_pos]
+            text_output += new_char
+        
+        else: 
+            text_output += character
+    
+    return text_output
+
+plaintext = input("Give me a text to encrypt: ")
+shift = int(input("How many letters do you want to shift your message by?: "))
+
+
+ciphertext = encrypt(plaintext, shift) 
+print("Encrypted Message: ", ciphertext)
